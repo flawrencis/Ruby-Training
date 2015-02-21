6e95c11e51ff1acb3f1dec57d7d2fb111c25f807
Francis Lawrence <franciselawrence@gmail.com>
2015-02-21 10:05:18 -0600

    Now trying to do a break thing inside.

--- 09_bubble-sort.rb
+++ 09_bubble-sort.rb
@@ -1,7 +1,8 @@
 def bubble_sort(array)
 	endpoint= array.length - 1
-	begin
-		checker=0
+	checker = 0
+	while i
+		puts "We're not there yet! The checker value is only #{checker}.\n"
 		array.each_with_index do |element,num|
 			comparison= element <=> array[num+1]
 			case comparison
@@ -12,7 +13,14 @@ def bubble_sort(array)
 				array[num]=array[num+1]
 				array[num+1]=element
 				end
+			puts "The checker value after evaluating this pair is: #{checker}.\n"
 			end
-		end #until checker==endpoint
-#function is not doing anything. irb takes me to blank line. And hitting enter above brings me to this indentation level (0)
+		puts "The checker value after evaluating the whole array this time is: #{checker}.\n"
+		if checker < endpoint
+			redo
+			else 
+			break
+			end
+		end
+	#Had to abandon "while" modifier at end of begin block. Replaced "begin" with "while" conditional.
 	end
