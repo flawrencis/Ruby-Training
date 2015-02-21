466b31a1258248d06113a66f1f869e020d917212
Francis Lawrence <franciselawrence@gmail.com>
2015-02-21 10:48:22 -0600

    Finished!
    I have no idea what is different between this and my first few versions
    except for the OR operator. But I don’t see how that would endlessly
    produce that same loop.

--- 09_bubble-sort.rb
+++ 09_bubble-sort.rb
@@ -2,10 +2,8 @@ def bubble_sort(array)
 	endpoint= array.length - 1
 	puts "The endpoint is #{endpoint}.\n"
 	checker = 0
-	while checker<endpoint
-		
-#		checker=0
-		puts "We're not there yet! The checker value is only #{checker}.\n"
+	begin
+#		puts "We're not there yet! The checker value is only #{checker}.\n"
 		array.each_with_index do |element,num|
 			
 			comparison= element <=> array[num+1]
@@ -13,25 +11,22 @@ def bubble_sort(array)
 				
 				when -1,0
 				checker+=1
-				puts "The checker value after evaluating the #{num+1}th pair is: #{checker}.\n"
+#				puts "The checker value after evaluating the #{num+1}th pair is: #{checker}.\n"
 				next
 				when 1
 				array[num]=array[num+1]
 				array[num+1]=element
 				end
-			puts "The checker value after evaluating the #{num+1}th pair is: #{checker}.\n"
+#			puts "The checker value after evaluating the #{num+1}th pair is: #{checker}.\n"
 			end
-		puts "The checker value after evaluating the whole array this time is: #{checker}.\n"
+#		puts "The checker value after evaluating the whole array this time is: #{checker}.\n"
 		if checker < endpoint
+			puts "Let's try again. So far, we've got #{checker} good pairs."
 			checker=0
 			redo
 			else 
 			break
 			end
-		end
+		end until checker==endpoint
 	puts "Your array is now...\n#{array}."
-	#Had to abandon "while" modifier at end of begin block. Replaced "begin" with "while" conditional.
 	end
-#puts "Give me your array."
-#arr= gets.chomp.to_a
-#bubble_sort(arr)
