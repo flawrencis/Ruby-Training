fd95ef0582a80d8314bbaa27749ec0fcfa4d7fc5
Francis Lawrence <franciselawrence@gmail.com>
2015-02-21 09:40:27 -0600

    Test 1: Fail

--- 09_bubble-sort.rb
+++ 09_bubble-sort.rb
@@ -14,4 +14,5 @@ def bubble_sort(array)
 				end
 			end
 		end until checker==endpoint
+#function is not doing anything. irb takes me to blank line. And hitting enter above brings me to this indentation level (0)
 	end
