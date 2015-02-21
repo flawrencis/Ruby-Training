8e20b2edc36a48fb6ec5ac1382b17a34407adfad
Francis Lawrence <franciselawrence@gmail.com>
2015-02-21 09:50:17 -0600

    Committing and syncing

--- 09_bubble-sort.rb
+++ 09_bubble-sort.rb
@@ -13,6 +13,6 @@ def bubble_sort(array)
 				array[num+1]=element
 				end
 			end
-		end until checker==endpoint
+		end #until checker==endpoint
 #function is not doing anything. irb takes me to blank line. And hitting enter above brings me to this indentation level (0)
 	end
