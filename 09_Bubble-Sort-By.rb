ffbc3a3fe9e78a7085094556c830ef8032738898
Francis Lawrence <franciselawrence@gmail.com>
2015-02-21 11:43:47 -0600

    Problems
    Very confused

--- 09_Bubble-Sort-By.rb
+++ 09_Bubble-Sort-By.rb
@@ -1,10 +1,11 @@
 def bubble_sort_by(array)
 	endpoint= array.length - 1
+	puts "The endpoint is: #{endpoint}"
 	begin
 		checker=0
 		array.each_with_index do |element,num|
-			
-			block_result= yield(array[num],array[num+1])
+#			puts "The length of the current element in the array is #{array[num].length}."
+			block_result= yield(num_array[num],num_array[num+1])
 			case block_result
 				
 				when -1 || 0
