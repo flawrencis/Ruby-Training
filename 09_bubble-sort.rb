59944eb0259250e8df30b65dbdc01d74062f3e7b
Francis Lawrence <franciselawrence@gmail.com>
2015-02-21 10:41:23 -0600

    It worked most recently. Made a couple changes.

--- 09_bubble-sort.rb
+++ 09_bubble-sort.rb
@@ -1,26 +1,37 @@
 def bubble_sort(array)
 	endpoint= array.length - 1
+	puts "The endpoint is #{endpoint}.\n"
 	checker = 0
-	while i
+	while checker<endpoint
+		
+#		checker=0
 		puts "We're not there yet! The checker value is only #{checker}.\n"
 		array.each_with_index do |element,num|
+			
 			comparison= element <=> array[num+1]
 			case comparison
-				when -1 || 0
+				
+				when -1,0
 				checker+=1
+				puts "The checker value after evaluating the #{num+1}th pair is: #{checker}.\n"
 				next
 				when 1
 				array[num]=array[num+1]
 				array[num+1]=element
 				end
-			puts "The checker value after evaluating this pair is: #{checker}.\n"
+			puts "The checker value after evaluating the #{num+1}th pair is: #{checker}.\n"
 			end
 		puts "The checker value after evaluating the whole array this time is: #{checker}.\n"
 		if checker < endpoint
+			checker=0
 			redo
 			else 
 			break
 			end
 		end
+	puts "Your array is now...\n#{array}."
 	#Had to abandon "while" modifier at end of begin block. Replaced "begin" with "while" conditional.
 	end
+#puts "Give me your array."
+#arr= gets.chomp.to_a
+#bubble_sort(arr)
