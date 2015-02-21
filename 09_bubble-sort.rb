5a3ff57a835702978d29893e07fdc75c1c5709d9
Francis Lawrence <franciselawrence@gmail.com>
2015-02-21 09:34:10 -0600

    First try

---
+++ 09_bubble-sort.rb
@@ -0,0 +1,17 @@
+def bubble_sort(array)
+	endpoint= array.length - 1
+	begin
+		checker=0
+		array.each_with_index do |element,num|
+			comparison= element <=> array[num+1]
+			case comparison
+				when -1 || 0
+				checker+=1
+				next
+				when 1
+				array[num]=array[num+1]
+				array[num+1]=element
+				end
+			end
+		end until checker==endpoint
+	end
