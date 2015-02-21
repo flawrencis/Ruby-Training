43344e3f7f7d8a551b1fc235689256faa84886a6
Francis Lawrence <franciselawrence@gmail.com>
2015-02-21 12:12:05 -0600

    Tweaked Sort, finished By
    I don’t actually know how it is that my bubble-sort worked. It’s kind
    of amazing! Why didn’t it have a problem when it compared the end of
    the line to nil? Oh, maybe 5<nil?

--- 09_Bubble-Sort-By.rb
+++ 09_Bubble-Sort-By.rb
@@ -3,17 +3,24 @@ def bubble_sort_by(array)
 	puts "The endpoint is: #{endpoint}"
 	begin
 		checker=0
+		puts "Okay, we are cycling through again!"
 		array.each_with_index do |element,num|
 #			puts "The length of the current element in the array is #{array[num].length}."
-			block_result= yield(num_array[num],num_array[num+1])
-			case block_result
-				
-				when -1 || 0
-				checker+=1
-				next
-				when 1
-				array[num]=array[num+1]
-				array[num+1]=element
+			inputy_1= array[num]
+			puts "inputy_1 is #{inputy_1}"
+			unless num==array.length-1
+				inputy_2= array[num+1]
+				puts "inputy_2 is #{inputy_2}"
+				block_result = yield(inputy_1,inputy_2)
+				puts block_result.to_s + " is the difference between the two elements just evaluated!"
+				if block_result <= 0
+					checker+=1
+					puts "The value for checker is now #{checker}."
+					else
+					array[num]=array[num+1]
+					array[num+1]=element
+					puts "The array is now #{array}!\n"
+					end
 				end
 			end
 		end until checker==endpoint
--- 09_bubble-sort.rb
+++ 09_bubble-sort.rb
@@ -3,16 +3,16 @@ def bubble_sort(array)
 	begin
 		checker=0
 		array.each_with_index do |element,num|
-			
-			comparison= element <=> array[num+1]
-			case comparison
-				
-				when -1 || 0
-				checker+=1
-				next
-				when 1
-				array[num]=array[num+1]
-				array[num+1]=element
+			unless num==endpoint
+				comparison= element <=> array[num+1]
+				case comparison
+
+					when -1 || 0
+					checker+=1
+					when 1
+					array[num]=array[num+1]
+					array[num+1]=element
+					end
 				end
 			end
 		end until checker==endpoint
