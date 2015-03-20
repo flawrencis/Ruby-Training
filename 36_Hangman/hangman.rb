2e653404c03be4df7aa3dc4aaed601ebf9744126
Francis Lawrence <franciselawrence@gmail.com>
2015-03-20 16:00:46 -0500

    Tired
    Still haven’t started file part.

--- 36_Hangman/hangman.rb
+++ 36_Hangman/hangman.rb
@@ -4,6 +4,7 @@ class Game
 	def initialize
 		puts "Welcome to Hangman!"
 		@the_word = Word.new
+		@lett_i_hash = @the_word.word_setup
 		@player = Player.new
 		@max_rounds = 8 
 		end
@@ -16,11 +17,17 @@ class Game
 		@round = 0
 		show_spaces(@round,@the_word.length)
 		while @round < @max_rounds
-			
+			puts "We have:"
 			show_spaces(@round,@the_word.length - @round)
+			puts "What would you like to guess now?"
+			lett_guess = get_guess
 			#this if clause and some other stuff below is no bueno. Need to properly set up the word.
 			if correct_guess? #need to define this method
 				lett_known << correct_guess
+				letter = correct_guess
+				lett_sym = letter.to_sym
+				lett_freq = @lett_freq_hash[lett_sym]
+				puts "You correctly guessed #{letter}. This letter appears #{lett_freq} times!"
 				else
 				hanging << rounds_array[error_num]
 				puts hanging
@@ -28,20 +35,34 @@ class Game
 			end
 		end
 	
-	def word_setup #could use uniq in this part
-		word_array_1 = @the_word.split
-		word_length = @the_word.length
-		lett_freq_hash = Hash.new
-		word_array_1.each do |letter|
-			lett_freq_hash
+	def correct_guess?
+		if #blah
+			return #value
+			elsif #blah
+			return false
+			end
+		end
 	
-	def show_spaces(round,length)
+	
+	def show_spaces(round,length,spaces)
 		spaces = String.new
+		#PUT THE INITIAL DEFINITION OF SPACES ELSEWHERE. ONE TIME. THEN, IN THE SHOW_SPACES, USE THE FOLLOWING CODE
+		=begin
+			lett_hsh = Hash with the letters as symbols followed by an array that gives their positions in the word
+			You set i_array = lett_hash[lett_sym]
+			You go i_array.each do |i|
+				spaces[i] = letter
+				end
+			done
+		=end
+		
 		for i in 1..length
 			spaces << " _"
 			end
+		lett_i_hash
+		i_of_known = 
 		spaces[i_of_known] = lett_known
-		puts spaces
+		return spaces
 		end
 
 	
@@ -64,6 +85,21 @@ class Word
 		choice = rand(0..usa_num)
 		the_word = usable_words[choice].chomp
 		puts "DEBUG: The word is #{the_word}, which is of class #{the_word.class}."
+		end
+	
+	def word_setup #could use uniq in this part
+		word_array_1 = @the_word.downcase.split
+		word_length = @the_word.length
+		@lett_freq_hash = Hash.new
+		lett_i_hash = Hash.new([]) #the index values of each letter in the word array
+		word_array_1.each_with_index do |letter,i|
+			lett_sym = letter.to_sym
+			@lett_freq_hash[lett_sym] ||= 0
+			@lett_freq_hash[lett_sym] += 1
+			lett_i_hash[lett_sym] << i
+			end
+		@guess_hash = Hash.new
+		return lett_i_hash
 		end
 	end
 
